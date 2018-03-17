1027aee81815ebacb328552ed2957a6f8c254397
咩~ <31565802+ehehee@users.noreply.github.com>
2018-03-17 20:21:21 +0800

    Update fa.py

--- fa.py
+++ fa.py
@@ -1,27 +1,59 @@
-from tools import read_reg
+def parse_convert(s):
+    """
+    :param s: 字符串
+    :return: 转换转义符之后的字符串
+    """
+    s = s.replace(r"\n", "\n")
+    s = s.replace(r"\r", "\r")
+    s = s.replace(r"\t", "\t")
+    return s
 
-class Nfanode(object):
-    def __init__(self):
-        self.nextnode = set()
+def read_reg(filename = 'reg.txt'):
+    """
+    :param filename: 文件名
+    :return: 返回一个每次迭代得到一行分割之后的二元组的生成器
+   """
+    with open(filename,'r') as reader:
+        comment = False
+        reg_type = {}
+        for line in reader:
+            line = line[:line.find('#')]    # 删除注释
+            line = parse_convert(line).strip()
+            if line == '"""' or line == "'''":
+                comment = not comment
+            if comment:
+                continue
+            if line.startswith('#') or line.find("::=") <= 0:
+                continue
+            l = list(line.split(" ::= "))
+            reg_type[l[0]] = l[1]
+    return reg_type
+            
     
-class Nfa(object):
-    stat = 0
-
-    def __init__(self):
-        self.stat = 0
-        self.nfa = dict()
-        self.end = False
-
-    def union(self,chr1,chr2,stat,nfa):
-        for i in range(6):
-            tmp = stat+i
-            nfa[tmp] = Nfanode()
-            nfa[tmp].nextnode['eps'] = list()
-        nfa[stat+1].nextnode['eps'] = [nfa[stat+2],nfa[stat+4]]
-
+  
+def infix2postfix(s):
+    s1 = s
+    opstack = []
+    chrstack = []
+    p = s1.find('[')
+    if p == -1 : return s1
+    while p<len(s):
+        q = s1.find(']',p)
+        chrstack += s1[p:q+1]
+        if q == -1 or q+1 == len(s): break
+        
+        if s1[q+1] != '*' :
+            opstack +=['.']
+        if s1[q+1] == '*':
+            opstack +=['*']
+        p = q+1
+        
+    return ("".join(chrstack) + "".join(list(reversed(opstack))))
+    
 if __name__ == '__main__':
-    meow = Nfa()
-    
+    tmp = read_reg()
+    for (d,x) in tmp.items():
+       print(d,infix2postfix(x))
          
         
 
