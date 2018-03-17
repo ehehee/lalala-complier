3e82cc0c0ace546de37c44b06c09fd93073ec3e0
咩~ <31565802+ehehee@users.noreply.github.com>
2018-03-17 20:23:59 +0800

    Update fa.py

--- fa.py
+++ fa.py
@@ -1,59 +1,39 @@
-def parse_convert(s):
-    """
-    :param s: 字符串
-    :return: 转换转义符之后的字符串
-    """
-    s = s.replace(r"\n", "\n")
-    s = s.replace(r"\r", "\r")
-    s = s.replace(r"\t", "\t")
-    return s
+from tools import read_reg
 
-def read_reg(filename = 'reg.txt'):
-    """
-    :param filename: 文件名
-    :return: 返回一个每次迭代得到一行分割之后的二元组的生成器
-   """
-    with open(filename,'r') as reader:
-        comment = False
-        reg_type = {}
-        for line in reader:
-            line = line[:line.find('#')]    # 删除注释
-            line = parse_convert(line).strip()
-            if line == '"""' or line == "'''":
-                comment = not comment
-            if comment:
-                continue
-            if line.startswith('#') or line.find("::=") <= 0:
-                continue
-            l = list(line.split(" ::= "))
-            reg_type[l[0]] = l[1]
-    return reg_type
-            
+class Nfanode(object):
+    def __init__(self):
+        self.nextnode = set()
     
-  
-def infix2postfix(s):
-    s1 = s
-    opstack = []
-    chrstack = []
-    p = s1.find('[')
-    if p == -1 : return s1
-    while p<len(s):
-        q = s1.find(']',p)
-        chrstack += s1[p:q+1]
-        if q == -1 or q+1 == len(s): break
+class Nfa(object):
+    stat = 0
+
+    def __init__(self):
+        self.stat = 0
+        self.nfa = dict()
+        self.end = False
+
+    def union(self,chr1,chr2,stat,nfa):
+        for i in range(6):
+            tmp = stat+i
+            nfa[tmp] = Nfanode()
+            nfa[tmp].nextnode['eps'] = list()
+        nstat = stat + 1
+        nfa[nstat].nextnode['eps'] = [stat+1,stat+3]
+        #for everyedge represent bt chr1 create the node 
+        #chr1 need to do something,from 'a-z'to ord(a)-ord(z)
+        for i in range(chr1):
+            nfa[stat+1].nextnode[i] = [stat+2]
         
-        if s1[q+1] != '*' :
-            opstack +=['.']
-        if s1[q+1] == '*':
-            opstack +=['*']
-        p = q+1
+        for i in range(chr2):
+            nfa[stat+1].nextnode[i] = [stat+2]
+       
+        nfa[stat+2].nextnode['eps'] = [stat+5]
+        nfa[stat+4].nextnode['eps'] = [stat+5]
         
-    return ("".join(chrstack) + "".join(list(reversed(opstack))))
-    
+
+
 if __name__ == '__main__':
-    tmp = read_reg()
-    for (d,x) in tmp.items():
-       print(d,infix2postfix(x))
+    meow = Nfa()
          
         
 
