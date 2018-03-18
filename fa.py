9f54911b395eb939dd77927f154e7ad7e911957c
ehehee <ehehe0606@gmail.com>
2018-03-18 14:42:16 +0800

    123

--- fa.py
+++ fa.py
@@ -2,7 +2,7 @@ from tools import read_reg,infix2postfix
 
 class Nfanode(object):
     def __init__(self):
-        self.nextnode = set()
+        self.nextnode = dict()
         self.end = False
     
 class Nfa(object):
@@ -13,52 +13,51 @@ class Nfa(object):
        
     #the relation between nfa and nfanode need to rethink 
     def union(self,chr1,chr2,stat):
+        nfa = dict()
         for i in range(6):
             tmp = stat+i
-            nfa_node[tmp] = Nfanode()
-            nfa_node[tmp].nextnode['eps'] = list()
+            nfa[tmp] = Nfanode()
+            nfa[tmp].nextnode['eps'] = list()
         nstat = stat + 1
-        nfa_node[nstat].nextnode['eps'] = [nstat+1,nstat+3]
+        nfa[nstat].nextnode['eps'] = [nstat+1,nstat+3]
         #for everyedge represent bt chr1 create the node 
         #chr1 need to do something,from 'a-z'to ord(a)-ord(z)
        
-        nfa_node[nstat+1].nextnode[chr1] = [nstat+2]
-        nfa_node[nstat+1].nextnode[chr2] = [nstat+2]
+        nfa[nstat+1].nextnode[chr1] = [nstat+2]
+        nfa[nstat+1].nextnode[chr2] = [nstat+2]
        
-        nfa_node[nstat+2].nextnode['eps'] = [nstat+5]
-        nfa_node[nstat+4].nextnode['eps'] = [nstat+5]
-        return 
+        nfa[nstat+2].nextnode['eps'] = [nstat+5]
+        nfa[nstat+4].nextnode['eps'] = [nstat+5]
+        return nfa 
 
 def generate_nfa():
     flname = 'test.txt'
     regex = read_reg(flname)
-    stat = 0
+    stat = 1
     unfa = Nfa()
     for (regtype,re) in regex.items():
         l = infix2postfix(re)
         symbolstack = list()
-        if '.' not in l and '*' not in l:
+        if '.' not in l:
             start  =  l.find('[')
             end = l.find(']')
             p = l.find('|')
             cnt = 1
             while p > 0 :
-                 symbolstack += l[start+1:p-1]
+                 symbolstack += l[start+1:p]
+                 print(symbolstack)
                  start = p 
                  p = l.find('|',start+1)
                  cnt += 1
                  if p == -1:
                      symbolstack += l[p:end]
-            
+                     break
+            symbolstack.remove('\\')
             if cnt != 0:
-                #unfa.union()
+                unfa.union(symbolstack[cnt-1],symbolstack[cnt],stat)
+                
+
                 #todo
-
-
-        
-            symbolstack += l[p,q-1]
-
-        print(re,infix2postfix(x))
 
 
 if __name__ == '__main__':
